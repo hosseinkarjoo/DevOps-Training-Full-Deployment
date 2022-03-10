e8bf998ceb4c3193a9f65a197045ed4373debd7c
Karjoo, Hossein <hosseinkarjoo@outlook.com>
2022-03-10 15:19:28 +0000

    test file

---
+++ test.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3.6
+
+import requests
+
+icanhazip = "http://ipv4.icanhazip.com"
+pubip = requests.get(icanhazip).text
+
+pub = pubip[:-1]
+
+myip = f"http://{pub}/pythonlogin"
+statuscode = requests.get(myip)
+
+if statuscode.status_code == 200:
+    print(f"OK{statuscode}")
+else:
+    print(f"ERROR{statuscode}")
+
