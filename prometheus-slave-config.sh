b2154f3a89614a34e626c08b812e7ac989875205
hosseinkarjoo <75358989+hosseinkarjoo@users.noreply.github.com>
2022-04-04 17:38:21 +0430

    Update prometheus-slave-config.sh

--- prometheus-slave-config.sh
+++ prometheus-slave-config.sh
@@ -4,5 +4,5 @@ SLAVE_PUB_IP=$(curl ipv4.icanhazip.com)
 SLAVE_PRV_IP=$(/usr/sbin/ip a show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
 
 
-sed -i '' -e "s/SLAVE_PUB_IP/$SLAVE_PUB_IP/" prometheus.yml
-sed -i '' -e "s/SLAVE_PRV_IP/$SLAVE_PRV_IP/" prometheus.yml
+sed -i -e "s/SLAVE_PUB_IP/$SLAVE_PUB_IP/" prometheus.yml
+sed -i -e "s/SLAVE_PRV_IP/$SLAVE_PRV_IP/" prometheus.yml
