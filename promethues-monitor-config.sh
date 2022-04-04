feed071b09cefc1acf7e0095e695fd0d978e374c
hosseinkarjoo <75358989+hosseinkarjoo@users.noreply.github.com>
2022-04-04 11:21:23 +0430

    Create promethues-monitor-config.sh

---
+++ promethues-monitor-config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+MONITOR_PUB_IP=$(curl ipv4.icanhazip.com)
+MONITOR_PRV_IP=$(ip a show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
+
+
+sed -i '' -e "s/MONITOR_PUB_IP/$MONITOR_PUB_IP/" prometheus.yml
+sed -i '' -e "s/MONITOR_PRV_IP/$MONITOR_PRV_IP/" prometheus.yml
