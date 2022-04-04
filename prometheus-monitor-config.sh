6cb3479c94d891c73c14536f73de0b43394b03e1
hosseinkarjoo <75358989+hosseinkarjoo@users.noreply.github.com>
2022-04-04 17:12:35 +0430

    Update prometheus-monitor-config.sh

--- prometheus-monitor-config.sh
+++ prometheus-monitor-config.sh
@@ -4,5 +4,5 @@ MONITOR_PUB_IP=$(curl ipv4.icanhazip.com)
 MONITOR_PRV_IP=$(ip a show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
 
 
-sed -i '' -e "s/MONITOR_PUB_IP/$MONITOR_PUB_IP/" prometheus.yml
-sed -i '' -e "s/MONITOR_PRV_IP/$MONITOR_PRV_IP/" prometheus.yml
+sed -i '' -e "s/MONITOR_PUB_IP/$MONITOR_PUB_IP/" ./prometheus.yml
+sed -i '' -e "s/MONITOR_PRV_IP/$MONITOR_PRV_IP/" ./prometheus.yml
