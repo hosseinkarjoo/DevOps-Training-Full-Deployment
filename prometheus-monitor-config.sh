f02ab26b28badf313a90212a9d9c6fa5b866974e
hosseinkarjoo <hosseinkarjoo@outlook.com>
2022-04-05 06:06:28 +0000

    shell ip

--- prometheus-monitor-config.sh
+++ prometheus-monitor-config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-MONITOR_PUB_IP=$(curl ipv4.icanhazip.com)
-MONITOR_PRV_IP=$(/usr/sbin/ip a show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
+MONITOR_PUB_IP=$(cat /var/lib/jenkins/workspace/Monitoring-stack/hosts-monitoring-stack.txt)
+SLAVE_PUB_IP=$(cat /var/lib/jenkins/workspace/Monitoring-stack/hosts-worker.txt)
 
 
 sed -i -e "s/MONITOR_PUB_IP/${MONITOR_PUB_IP}/" ./prometheus.yml
-sed -i -e "s/MONITOR_PRV_IP/${MONITOR_PRV_IP}/" ./prometheus.yml
+sed -i -e "s/SLAVE_PUB_IP/${SLAVE_PUB_IP}/" ./prometheus.yml
