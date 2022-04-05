565b7d40b4af68c995488355e4344bd8b39f1d6b
hosseinkarjoo <hosseinkarjoo@outlook.com>
2022-04-05 06:44:20 +0000

    update

--- prometheus-monitor-config.sh
+++ prometheus-monitor-config.sh
@@ -6,3 +6,4 @@ SLAVE_PUB_IP=$(cat /var/lib/jenkins/workspace/Monitoring-stack/hosts-worker.txt)
 
 sed -i -e "s/MONITOR_PUB_IP/${MONITOR_PUB_IP}/" ./prometheus.yml
 sed -i -e "s/SLAVE_PUB_IP/${SLAVE_PUB_IP}/" ./prometheus.yml
+sed -i -e "s/prometheus:9090/${MONITOR_PUB_IP}:9090/" ./datasource.yml
